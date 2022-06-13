8e76798e2d0a2ed2d00469677a66d3e9589c456c
Lucas Neuhaus <lucas@neuhaus.dev>
2022-06-13 10:50:11 +0200

    final sollutions

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -12,8 +12,76 @@ const IconInput = ({
   width = 250,
   size,
   placeholder,
+  delegeted,
 }) => {
-  return 'TODO';
+  return (
+    <Wrapper style={{
+      "--width": width + "px",
+      "--height": size === "small" ? "24px" : "36px",
+      "--icon-size": size === "small" ? "16px" : "24px",
+      "--font-size": size === "small" ? "0.875rem" : "1.125rem",
+      "--border-size": size === "small" ? "1px" : "2px",
+      "--icon-padding": size === "small" ? "24px" : "38px",
+    }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <BaseInput placeholder={placeholder} {...delegeted} />
+      <IconWrapper>
+        <Icon id={icon} size={size === "small" ? 16 : 22} strokeWidth={2} />
+      </IconWrapper>
+    </Wrapper>
+
+
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: var(--width);
+`
+
+const BaseInput = styled.input`
+  width: 100%;
+  height: var(--height);
+  padding-left: var(--icon-padding);
+  border: none;
+  border-bottom: var(--border-size) solid ${COLORS.black};
+
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  font-size: var(--font-size);
+
+  &:focus {
+    outline-offset: 2px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top:0;
+  bottom: 0;
+  left: 3px;
+  height: var(--icon-size);
+  margin: auto;
+  color: ${COLORS.gray700};
+
+  ${BaseInput}:hover + & {
+    color: ${COLORS.black};
+  }
+
+  ${BaseInput}:focus + & {
+    color: ${COLORS.black};
+  }
+`
+
+
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,61 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PressentationalBit>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id={"chevron-down"} strokeWidth={2} size={24} />
+        </IconWrapper>
+      </PressentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+
+const PressentationalBit = styled.div`
+  padding: 12px 16px;
+  padding-right: 52px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+
+  ${NativeSelect}:focus-visible + & {
+    outline: 2px dotted #212121;
+    outline: 5px auto -moz-focus-ring-color;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  height: 24px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  pointer-events: none;
+`
+
 
 export default Select;
